3c0f02bc2783b7c9612a466f3f0677a9f0b3b7d7
rafaelgeronimo <rafaelgeronimo@outlook.com>
2019-04-03 09:23:10 -0300

    feat:pacotes e primeira pagina

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import style from "./style.scss";
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="style.header">
+        <ul>
+          <li>
+            <Link to="/">Root</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+        </ul>
+      </header>
+    );
+  }
+}
